5c30bbc7b32b78bd06946bba4543e797fc6966de
chrisdane <cdanek@awi.de>
2020-01-23 14:03:15 +0100

    added namelists

---
+++ namelist.plot.r
@@ -0,0 +1,341 @@
+# input for plot.echam.r
+
+# host options
+machine <- system("hostname -f", intern=T)
+message(paste0("Run on ", machine, ":"))
+if (regexpr("ollie", machine) != -1 ||
+    regexpr("prod-", machine) != -1 ||
+    regexpr("fat-", machine) != -1) {
+    machine_tag <- "ollie"
+    homepath <- "~/scripts/r"
+    workpath <- "/work/ollie/cdanek"
+} else if (regexpr("hpc.dkrz", machine) != -1) {
+    machine <- substr(machine, 1, regexpr(".hpc.dkrz", machine) - 1)
+    machine_tag <- "mistral"
+    homepath <- "~/scripts/r"
+    #workpath <- "/work/ba0941/a270073"
+    workpath <- "/work/ab0246/a270073"
+} else {
+    message(paste0("   (unknown machine, use default paths)"))
+    homepath <- "~/scripts/r"
+    workpath <- homepath
+}
+message(paste0("   homepath = ", homepath))
+message(paste0("   workpath = ", workpath))
+
+# options across settings
+# echam:
+#mode <- "fldmean" 
+#mode <- "timmean" 
+#mode <- "volint"
+# fesom:
+#mode <- "moc_depth"
+#mode <- "moc_ts"
+#mode <- "mean"
+#mode <- "sum"
+#mode <- "area"
+mode <- "depth"
+
+# =====================================
+# 1 setting
+if (F) { # awi-esm-1-1-lr hist
+    #prefixes <- "hist_echam6_echammon_awi-esm-1-1-lr"
+    prefixes <- "hist_echam6_echammon_yearmean_awi-esm-1-1-lr"
+    models <- "echam6"
+    names_short <- "hist_wrt_1961_90"
+    names_legend <- "historical"
+    fromsf <- 1850
+    tosf <- 2014
+    varnames_in <- "temp2"
+    n_mas <- 1
+    cols <- "#E41A1C"
+    remove_mean_froms <- 1961
+    remove_mean_tos <- 1990
+
+# =====================================
+# 2 settings
+} else if (T) { # awi-esm-1-1-lr 1pct 4CO2
+    #models <- rep("echam6", t=2)
+    models <- rep("fesom", t=2)
+    if (T) {
+        prefixes <- c("awi-esm-1-1-lr_1percCO2_monthly_mean",
+                      "awi-esm-1-1-lr_4CO2_monthly_mean")
+    }
+    names_short <- c("1pctCO2", "abrupt-4xCO2") 
+    if (T) {
+        varnames_in <- rep("mlotst", t=2)
+        varnames_in <- rep("thetao", t=2)
+        #varnames_in <- rep("so", t=2)
+        #varnames_in <- rep("potdens", t=2)
+        #areas <- rep("LSstolpe18", t=2)
+        areas <- rep("GIN2", t=2)
+        depths <- rep("0-5900", t=2)
+        remove_mean_froms <- c(1850, 1850)
+        remove_mean_tos <- remove_mean_froms
+        depth_fromsp <- rep(-3500, t=2)
+    }
+    if (T) { # transient pi last 100
+        fromsf <- c(1850, 1850)
+        tosf <- c(2099, 2099)
+    } else if (F) { # ltm last 30
+        fromsf <- c(2070, 2070)
+        tosf <- c(2099, 2099)
+    }
+    #n_mas <- rep(60, t=2)
+    n_mas <- rep(36, t=2)
+    #n_mas <- rep(1, t=2)
+    if (T) {
+        names_legend <- c(eval(substitute(expression(paste("1%CO"[2])))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2])))))
+    } else if (F) {
+        names_legend <- c(eval(substitute(expression(paste("1%CO"[2], " last 30 years mean")))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " last 30 years mean")))))
+    } else if (F) {
+        names_legend <- c(eval(substitute(expression(paste("1%CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsp[3], from=fromsp[1], to=tosp[1]))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsp[4], from=fromsp[2], to=tosp[2]))))
+    }
+
+# =====================================
+# 3 settings
+} else if (F) { # awi-esm-1-1-lr deck anomlies vs pi
+    models <- rep("echam6", t=3)
+    #models <- rep("fesom", t=3)
+    if (T) {
+        prefixes <- c("hist_minus_piControl_echam6_echammon_awi-esm-1-1-lr",
+                      "1percCO2_minus_piControl_echam6_echammon_awi-esm-1-1-lr",
+                      "4CO2_minus_piControl_echam6_echammon_awi-esm-1-1-lr")
+    } else if (F) {
+        prefixes <- c("awi-esm-1-1-lr_hist_minus_piControl_monthly_mean",
+                      "awi-esm-1-1-lr_1percCO2_minus_piControl_monthly_mean",
+                      "awi-esm-1-1-lr_4CO2_minus_piControl_monthly_mean")
+    }
+    names_short <- c("hist", "1pctCO2", "abrupt-4xCO2") 
+    if (T) {
+        varnames_in <- rep("temp2", t=3)
+    } else if (F) {   
+        varnames_in <- rep("tos", t=3)
+        postpaths <- paste0(workpath, "/post/", models, "/regular_grid/ltm/", mode, "/", varnames_in)
+        reg_dxs <- reg_dys <- rep("0.250", t=3)
+    }
+    fromsf <- c(1985, 2070, 2070)
+    tosf <- c(2014, 2099, 2099)
+    if (F) {
+        names_legend <- c("historical", 
+                          eval(substitute(expression(paste("1%CO"[2])))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2])))))
+    } else if (T) {
+        names_legend <- c(paste0("historical ", fromsf[1], "-", tosf[1], " mean minus piControl"),
+                          #paste0("historical ", fromsp[2], "-", tosp[2], " mean"),
+                          eval(substitute(expression(paste("1%CO"[2], " last 30 years mean minus piControl")))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " last 30 years mean minus piControl")))))
+    }
+
+# =====================================
+# 4 settings
+} else if (F) { # awi-esm-1-1-lr deck
+    #models <- rep("echam6", t=4)
+    models <- rep("fesom", t=4)
+    if (F) {
+        prefixes <- c("piControl_echam6_echammon_awi-esm-1-1-lr",
+                      "hist_echam6_echammon_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echammon_awi-esm-1-1-lr",
+                      "4CO2_echam6_echammon_awi-esm-1-1-lr")
+    } else if (F) {
+        prefixes <- c("piControl_echam6_echam_awi-esm-1-1-lr",
+                      "hist_echam6_echam_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echam_awi-esm-1-1-lr",
+                      "4CO2_echam6_echam_awi-esm-1-1-lr")
+    } else if (F) {
+        prefixes <- c("piControl_echam6_echam_yearmean_awi-esm-1-1-lr",
+                      "hist_echam6_echam_yearmean_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echam_yearmean_awi-esm-1-1-lr",
+                      "4CO2_echam6_echam_yearmean_awi-esm-1-1-lr")
+    } else if (F) {
+        prefixes <- c("piControl_echam6_echammon_yearmean_awi-esm-1-1-lr",
+                      "hist_echam6_echammon_yearmean_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echammon_yearmean_awi-esm-1-1-lr",
+                      "4CO2_echam6_echammon_yearmean_awi-esm-1-1-lr")
+    } else if (F) {
+        prefixes <- c("piControl_echam6_aeroptmon_awi-esm-1-1-lr",
+                      "hist_echam6_echammon_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echammon_awi-esm-1-1-lr",
+                      "4CO2_echam6_echammon_awi-esm-1-1-lr")
+    } else if (T) { # fesom
+        prefixes <- c("awi-esm-1-1-lr_piControl_monthly_mean",
+                      "awi-esm-1-1-lr_hist_monthly_mean",
+                      "awi-esm-1-1-lr_1percCO2_monthly_mean",
+                      "awi-esm-1-1-lr_4CO2_monthly_mean")
+    }
+    names_short <- c("piControl", "hist", "1pctCO2", "abrupt-4xCO2") 
+    text_cols <- c("black", "#E41A1C", "#377EB8", "#1B9E77")
+    scatterpchs <- c(4, 16, 16, 16)
+    codes <- rep("", t=4)
+    if (F) {
+        varnames_in <- rep("temp2", t=4)
+        codes <- c(167, "", "", "")
+    } else if (F) {
+        varnames_in <- rep("srad0", t=4)
+    } else if (F) {
+        varnames_in <- rep("toa_imbalance", t=4)
+    } else if (F) {
+        varnames_in <- rep("tau_aero_550", t=4)
+        codes <- c(11, "", "", "")
+    } else if (F) {
+        varnames_in <- rep("srad0d", t=4)
+        codes <- c(184, "", "", "")
+    } else if (F) { # fesom
+        varnames_in <- rep("MOCw", t=4)
+        areas <- rep("NA", t=4)
+        depths <- rep("0-5900", t=4)
+        moc_lats <- c(26.5, 41)
+    } else if (F) {
+        varnames_in <- rep("siarean", t=4)
+        areas <- rep("arctic", t=4)
+    } else if (T) {
+        varnames_in <- rep("mlotst", t=4)
+        varnames_in <- rep("thetao", t=4)
+        #varnames_in <- rep("so", t=4)
+        #varnames_in <- rep("potdens", t=4)
+        areas <- rep("LSstolpe18", t=4)
+        #areas <- rep("GIN2", t=4)
+        depths <- rep("0-5900", t=4)
+        remove_mean_froms <- c(1849, 1850, 1850, 1850)
+        remove_mean_tos <- remove_mean_froms
+        depth_fromsp <- rep(-3500, t=4)
+    } else if (F) {
+        varnames_in <- rep("tos", t=4)
+        postpaths <- paste0(workpath, "/post/", models, "/regular_grid/ltm/", mode, "/", varnames_in)
+        reg_dxs <- reg_dys <- rep("0.250", t=4)
+    }
+    if (F) { # transient pi last 100
+        fromsf <- c(1842, 1850, 1850, 1850)
+        tosf <- c(1941, 2014, 2099, 2099)
+        froms_shift <- c(1842-91, NA, NA, NA)
+        #fromsp <- c(1849-99, 2014-29, 2099-29, 2099-29)
+        #tosp <- c(1849, 2014, 2099, 2099)
+        fromsp <- c(1849-99, 1850, 1850, 1850)
+        tosp <- c(1849, 2014, 2099, 2099)
+    } else if (T) { # tranient pi last 30
+        fromsf <- c(1912, 1850, 1850, 1850)
+        tosf <- c(1941, 2014, 2099, 2099)
+        froms_shift <- c(1912-91, NA, NA, NA)
+        fromsp <- c(1849-29, 1850, 1850, 1850)
+        tosp <- c(1849, 2014, 2099, 2099)
+    } else if (F) { # ltm last 30
+        fromsf <- c(1912, 1985, 2070, 2070)
+        tosf <- c(1941, 2014, 2099, 2099)
+    }
+    #seasonsf <- rep("Jan-Dec", t=4)
+    #seasonsp <- seasonsf
+    #seasonsp <- rep("JFM", t=4) 
+    #seasonsp <- rep("Mar", t=4)
+    #seasonsp <- rep("Jul", t=4)
+    #n_mas <- rep(60, t=4)
+    n_mas <- rep(36, t=4)
+    #n_mas <- rep(1, t=4)
+    if (T) {
+        names_legend <- c("piControl", 
+                          "historical", 
+                          eval(substitute(expression(paste("1%CO"[2])))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2])))))
+    } else if (F) {
+        names_legend <- c("piControl last 30 years mean",
+                          #"piControl last 100 years mean",
+                          paste0("historical ", fromsf[2], "-", tosf[2], " mean"),
+                          #paste0("historical ", fromsp[2], "-", tosp[2], " mean"),
+                          eval(substitute(expression(paste("1%CO"[2], " last 30 years mean")))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " last 30 years mean")))))
+    } else if (F) {
+        names_legend <- c(paste0("piControl ", seasonsp[1], " ", fromsp[1], "-", tosp[1]),
+                          paste0("historical ", seasonsp[2], " ", fromsp[2], "-", tosp[2]),
+                          eval(substitute(expression(paste("1%CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsp[3], from=fromsp[3], to=tosp[3]))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsp[4], from=fromsp[4], to=tosp[4]))))
+    }
+    
+} else if (F) { # awi-esm-1-1-lr deck and lgm
+    if (F) {
+        prefixes <- c("hist_echam6_echammon_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echammon_awi-esm-1-1-lr",
+                      "4CO2_echam6_echammon_awi-esm-1-1-lr",
+                      "MM_01.01_echam_awi-esm-1-1-lr_lgm")
+    } else if (T) {
+        prefixes <- c("hist_echam6_echam_awi-esm-1-1-lr",
+                      "1percCO2_echam6_echam_awi-esm-1-1-lr",
+                      "4CO2_echam6_echam_awi-esm-1-1-lr",
+                      "MM_01.01_echam_awi-esm-1-1-lr_lgm")
+    }
+    models <- rep("echam6", t=4)
+    names_short <- c("hist", "1pctCO2", "abrupt-4xCO2", "lgm") 
+    fromsf <- c(1850, 1850, 1850, 3537)
+    #fromsf <- c(rep(1985, t=3), 3843)
+    #fromsf <- c(1985, 2070, 2070, 3843)
+    #tosf <- c(rep(2014, t=3), 3872)
+    tosf <- c(2014, 2099, 2099, 3872)
+    seasonsf <- rep("Jan-Dec", t=4)
+    if (F) {
+        names_legend <- c("historical", 
+                          eval(substitute(expression(paste("1%CO"[2])))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2])))),
+                          "LGM")
+    } else if (T) {
+        names_legend <- c(paste0("historical ", seasonsf[1], " ", fromsf[1], "-", tosf[1]),
+                          eval(substitute(expression(paste("1%CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsf[2], from=fromsf[2], to=tosf[2]))),
+                          eval(substitute(expression(paste("abrupt-4" %*% "CO"[2], " ", season, " ", from, "-", to)),
+                                          list(season=seasonsf[3], from=fromsf[3], to=tosf[3]))),
+                          paste0("LGM ", seasonsf[4], " ", fromsf[4], "-", tosf[4]))
+    }
+    n_mas <- rep(60, t=4)
+    #varnames_in <- rep("temp2", t=4)
+    varnames_in <- rep("srad0", t=4)
+    
+}
+# ==================================================
+
+# script options
+squeeze_data <- T # drop dims with length=1 (e.g. lon and lat after fldmean)
+
+# general plot options
+add_title <- T
+add_legend <- T
+p <- setDefaultPlotOptions(plot_type="png", 
+                           #plot_type="pdf",
+                           family_png="Droid Sans Mono", 
+                           family_pdf="Droid Sans Mono"
+                           )
+                           #family_pdf="CM Roman")
+
+# time series plot options
+# woa13 seasons: "JFM" "AMJ" "JAS" "OND"
+# other seasons: "Jan-Dec" "DJF" "MAM" "JJA" "SON" "JJ"
+# months: "Feb" "Jul" "Jan"  "Jul"
+add_xgrid <- F
+add_ygrid <- F
+add_zeroline <- T
+add_unsmoothed <- F
+add_smoothed <- T
+# time:
+add_sd <- F
+add_linear_trend <- T
+add_nonlinear_trend <- F
+scale_ts <- F
+add_first_data_point <- F
+add_data_right_yaxis_ts <- F
+plot_scatter_var1_vs_var2 <- T
+add_legend_right_yaxis <- F
+add_data_right_yaxis_ts_mon <- F
+# time vs depth:
+add_ts_to_time_vs_depth <- T
+
+plot_redfit <- F
+
+# map plot options
+proj <- "rectangular" #"rectangular"
+add_land <- T
+reorder_lon_from_0360_to_180180 <- T
+add_grid <- F
+
---
+++ namelist.post.r
@@ -0,0 +1,190 @@
+# input for post.echam.r
+
+# host options
+machine <- system("hostname -f", intern=T)
+message(paste0("Run on ", machine, ":"))
+if (regexpr("ollie", machine) != -1 ||
+    regexpr("prod-", machine) != -1 ||
+    regexpr("fat-", machine) != -1) {
+    machine_tag <- "ollie"
+    homepath <- "~/scripts/r"
+    workpath <- "/work/ollie/cdanek"
+} else if (regexpr("hpc.dkrz", machine) != -1) {
+    machine <- substr(machine, 1, regexpr(".hpc.dkrz", machine) - 1)
+    machine_tag <- "mistral"
+    homepath <- "~/scripts/r"
+    #workpath <- "/work/ba0941/a270073"
+    workpath <- "/work/ab0246/a270073"
+} else {
+    message(paste0("   (unknown machine, use default paths)"))
+    homepath <- "~/scripts/r"
+    workpath <- homepath
+}
+message(paste0("   homepath = ", homepath))
+message(paste0("   workpath = ", workpath))
+
+verbose <- 1 # 0,1
+clean <- T # remove tmp files
+cdo_silent <- "" # "-s" for silent or ""
+cdo_force <- T # redo cdo command although outout file already exists 
+cdo_OpenMP_threads <- "-P 4" # "-P n" or "" (will be irgnored on commands that do not support OMP)
+cdo_wout_loop <- T # keep true; run one cdo command on all files or loop through all files
+set_rel_time <- T # conversion from absolute (default) to relative time
+add_my_time <- F # my time for ts output (needs package ncdf4) 
+
+# ======================================================
+# 1 setting
+if (F) { # old hist
+    datapaths <- "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/hist/outdata/echam"
+    fpatterns <- "hist_echam6_echammon_<YYYY><MM>.nc"
+    #fvarnames <- "temp2"
+    fvarnames <- "srad0d"
+    models <- "echam6"
+    froms <- 1850
+    tos <- 1851
+    #season_inds <- list(c(12, 1, 2))
+    modes <- "fldmean"
+    #modes <- "timmean"
+    suffixs <- "dynveg"
+    
+} else if (T) { # pi
+    datapaths <- "/work/ab0246/a270073/awicm-test/CMIP6/CMIP_PMIP/dynveg_true/piControl/outdata/echam" # 1543:1941
+    if (T) {
+        fpatterns <- "piControl_echam6_echammon_<YYYY><MM>.grb"
+        fvarnames <- "temp2"
+        codes <- 167
+        #fvarnames <- "srad0d"
+        #codes <- 184
+    } else if (F) {
+        fpatterns <- "piControl_echam6_echam_<YYYY><MM>.grb"
+        #fvarnames <- "trad0"
+        #codes <- 179
+        fvarnames <- "srad0"
+        codes <- 178
+    } else if (F) {
+        fpatterns <- "piControl_echam6_aeroptmon_<YYYY><MM>.grb"
+        fvarnames <- "tau_aero_550_pt"
+        codes <- 11
+    }
+    models <- "echam6"
+    froms <- 1912 # last 30 years: 1912:1941; last 100 years: 1842:1941 
+    tos <- 1941
+    #modes <- "fldmean"
+    modes <- "timmean"
+    suffixs <- "awi-esm-1-1-lr"
+
+} else if (F) { # xiaoxu
+    datapaths <- "/mnt/lustre02/work/ba0989/a270064/esm-experiments/lgm_anm/outdata/echam" # 3537:2872 (n=336)
+    fpatterns <- "MM_<YYYY>01.01_echam.nc"
+    #fvarnames <- "temp2"
+    fvarnames <- "trad0"
+    models <- "echam6"
+    froms <- 3537 # last 30 years start from 3843
+    tos <- 3872
+    #season_inds <- list(c(12, 1, 2)) # DJF
+    #season_inds <- list(c(3, 4, 5)) # MAM
+    #season_inds <- list(c(6, 7, 8)) # JJA
+    #season_inds <- list(c(9, 10, 11)) # SON
+    #modes <- "timmean" 
+    modes <- "fldmean"
+    suffixs <- "awi-esm-1-1-lr_lgm"
+
+# ======================================================
+# 2 settings
+} else if (F) {
+    datapaths <- c("/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/hist/outdata/echam",
+                   "/work/ab0246/a270073/awicm-test/CMIP6/CMIP_PMIP/dynveg_true/hist_LUH/outdata/echam")
+    fpatterns <- c(#"hist5_echam6_echammon_<YYYY>.grb",
+                   "hist_echam6_echammon_<YYYY><MM>.nc",
+                   "hist_LUH_echam6_echammon_<YYYY><MM>.grb")
+    fvarnames <- c("temp2", "temp2")
+    codes <- c(NA, 167) # necessary for grb files
+    models <- c("echam6", "echam6") # for check if `cdo -t echam6` can be used
+    froms <- c(1850, 1850)
+    tos <- c(1851, 1851)
+    #modes <- c("fldmean", "fldmean")
+    modes <- c("timmean", "timmean")
+    suffixs <- c("dynveg_noLUH", "dynveg_LUH")
+
+} else if (F) {
+    datapaths <- c("/work/ab0246/a270073/awicm-test/CMIP6/CMIP_PMIP/dynveg_true/1percCO2/outdata/echam",
+                   "/work/ab0246/a270073/awicm-test/CMIP6/CMIP_PMIP/dynveg_true/4CO2/outdata/echam")
+    fpatterns <- c("1percCO2_echam6_echammon_<YYYY><MM>.nc",
+                   "4CO2_echam6_echammon_<YYYY><MM>.nc")
+    fvarnames <- c("temp2", "temp2")
+    models <- c("echam6", "echam6") # for check if `cdo -t echam6` can be used
+    froms <- c(1850, 1850)
+    #tos <- c(1859, 1859)
+    tos <- c(2099, 2099)
+    modes <- c("fldmean", "fldmean")
+    suffixs <- c("dynveg", "dynveg")
+
+# ======================================================
+# 3 settings
+} else if (F) { # deck hist 1pct 4co2
+    datapaths <- c("/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/hist/outdata/echam",
+                   "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/1percCO2/outdata/echam",
+                   "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/4CO2/outdata/echam")
+    fpatterns <- c("hist_echam6_echam_<YYYY><MM>.nc",
+                   "1percCO2_echam6_echam_<YYYY><MM>.nc",
+                   "4CO2_echam6_echam_<YYYY><MM>.nc")
+    #fvarnames <- c("temp2", "temp2", "temp2")
+    #fvarnames <- rep("srad0d", t=3)
+    #fvarnames <- rep("srad0", t=3)
+    fvarnames <- rep("trad0", t=3)
+    models <- c("echam6", "echam6", "echam6") # for check if `cdo -t echam6` can be used
+    froms <- c(1850, 1850, 1850)
+    #froms <- c(1985, 1985, 1985)
+    #froms <- c(1985, 2070, 2070)
+    #tos <- c(1851, 1851, 1851)
+    #tos <- c(2014, 2014, 2014)
+    tos <- c(2014, 2099, 2099)
+    #season_inds <- list(c(12, 1, 2), c(12, 1, 2), c(12, 1, 2)) # DJF
+    #season_inds <- list(3:5, 3:5, 3:5) # MAM
+    #season_inds <- list(6:8, 6:8, 6:8) # JJA
+    #season_inds <- list(9:11, 9:11, 9:11) # SON
+    modes <- c("fldmean", "fldmean", "fldmean")
+    #modes <- c("timmean", "timmean", "timmean")
+    suffixs <- rep("awi-esm-1-1-lr", t=3)
+
+} else if (F) { # deck pi hist 1pct 4cos2
+    datapaths <- c("/work/ab0246/a270073/awicm-test/CMIP6/CMIP_PMIP/dynveg_true/piControl/outdata/echam",
+                   "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/hist/outdata/echam",
+                   "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/1percCO2/outdata/echam",
+                   "/work/ba0989/a270077/CMIP6_PMIP4/a270073/CMIP6/CMIP_PMIP/dynveg_true/old/4CO2/outdata/echam")
+    if (F) {
+        fpatterns <- c("piControl_echam6_echam_<YYYY><MM>.grb",
+                       "hist_echam6_echam_<YYYY><MM>.nc",
+                       "1percCO2_echam6_echam_<YYYY><MM>.nc",
+                       "4CO2_echam6_echam_<YYYY><MM>.nc")
+    } else if (T) {
+        fpatterns <- c("piControl_echam6_echammon_<YYYY><MM>.grb",
+                       "hist_echam6_echammon_<YYYY><MM>.nc",
+                       "1percCO2_echam6_echammon_<YYYY><MM>.nc",
+                       "4CO2_echam6_echammon_<YYYY><MM>.nc")
+    } else if (F) {
+        fpatterns <- c("piControl_echam6_aeroptmon_<YYYY><MM>.grb",
+                       "hist_echam6_echammon_<YYYY><MM>.nc",
+                       "1percCO2_echam6_echammon_<YYYY><MM>.nc",
+                       "4CO2_echam6_echammon_<YYYY><MM>.nc")
+    }
+    #fvarnames <- rep("temp2", t=4)
+    #fvarnames <- rep("srad0d", t=3)
+    #fvarnames <- rep("srad0", t=3)
+    #fvarnames <- rep("trad0", t=3)
+    fvarnames <- rep("tau_aero_550", t=4)
+    codes <- rep(11, t=4)
+    models <- rep("echam6", t=4) # for check if `cdo -t echam6` can be used
+    froms <- c(1842, 1850, 1850, 1850) # pi last 30 years: 1912:1941; pi last 100 years: 1842:1941
+    tos <- c(1941, 2014, 2099, 2099)
+    #tos <- froms
+    #season_inds <- list(c(12, 1, 2), c(12, 1, 2), c(12, 1, 2)) # DJF
+    #season_inds <- list(3:5, 3:5, 3:5) # MAM
+    #season_inds <- list(6:8, 6:8, 6:8) # JJA
+    #season_inds <- list(9:11, 9:11, 9:11) # SON
+    #modes <- rep("fldmean", t=4)
+    #modes <- rep("timmean", t=4)
+    modes <- rep("volint", t=4)
+    suffixs <- rep("awi-esm-1-1-lr", t=4)
+}
+
