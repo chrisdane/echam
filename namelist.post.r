9c9066f0e1f87ae68e56446456a8599fe5f4b7bf
Christopher Danek <cdanek@stan1.awi.de>
2020-03-04 16:25:48 +0100

    asd

--- namelist.post.r
+++ namelist.post.r
@@ -103,7 +103,7 @@ if (F) { # old hist
     modes <- "fldmean"
     prefixs <- "awi-esm-1-1-lr_lgm"
 
-} else if (T) { # Hol-Tx10 on paleosrv
+} else if (F) { # Hol-Tx10 on paleosrv
     datapaths <- "/scratch/simulation_database/incoming/Hol-Tx10/output"
     models <- "echam5"
     #fpatterns <- "Hol-Tx10_echam5_main_mm_<YYYY><MM>.nc"
@@ -153,7 +153,7 @@ if (F) { # old hist
     cdo_codetables <- "~/scripts/r/echam/wiso/CODES.WISO"
     cdo_partablesn <- "~/scripts/r/echam/wiso/CODES.WISO.txt"
 
-} else if (F) { # Hol-T on stan
+} else if (T) { # Hol-T on stan
     datapaths <- "/ace/user/cdanek/out/cosmos-aso-wiso/Hol-T/outdata/echam5"
     models <- "echam5"
     ftypes <- "l" # "f" for files (default) or "l" for links
@@ -172,7 +172,8 @@ if (F) { # old hist
     #levs_out <- 2
     #fvarnames <- "aprt_times_temp2"
     #fvarnames <- "aprt_times_tsurf"
-    fvarnames <- "ptemp"
+    #fvarnames <- "ptemp"
+    fvarnames <- "ptsurf"
     modes <- "select"
     #modes <- "fldmean"
     #modes <- "yearsum"
